4239e85946b8b35a49a6d02a3537c56c9daa828e
joseph <joseph@pc-haut.home>
2019-11-16 00:34:30 +0100

    Ajout de la lettre

--- src/App.js
+++ src/App.js
@@ -3,13 +3,36 @@ import './App.css';
 import Locker from './components/Locker';
 //import 'semantic-ui-css/semantic.min.css';
 import 'semantic-ui-css-offline';
-import { Grid, Icon, Modal, Image } from 'semantic-ui-react';
+import { Grid, Icon, Modal, Header, Image, Button } from 'semantic-ui-react';
+
+const lockers = [ {
+	color: 'blue',
+        type: 'digits',
+        digits: 4,
+        code: [7, 1, 9, 2]
+}, {
+        color: 'red',
+        type: 'digits',
+	digits: 4,
+        code: [3, 8, 7, 6]
+}, {
+        color: 'black',
+        type: 'pass',
+        digits: 3,
+        code: ['super', 'vous', 'chauffez']
+}, {
+        color: 'violet',
+        type: 'digits',
+        digits: 3,
+        code: [1, 5, 5]
+} ]
 
 class App extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
+			letterOpened: true,
 			lockers: null,
 			locks: null,//Array(lockers.length).fill(null),
 			locked: true
@@ -19,7 +42,7 @@ class App extends React.Component {
 			return r.json();
 		})
 		.then((j) => {
-			this.setState({ lockers: j.codes, locks: Array(j.codes.length) });
+			this.setState({ lockers: j.codes, locks: Array(j.codes.length).fill(null) });
 		});
 	}
 
@@ -32,12 +55,43 @@ class App extends React.Component {
 		}
 	}
 
+	handleLetterOpen = () => this.setState({ letterOpened: true });
+
+	handleLetterClose = () => this.setState({ letterOpened: false });
+
 	render() {
-		const { locked, lockers } = this.state;
+		const { letterOpened, locked, lockers } = this.state;
   return (
     <div className="App">
       <header className="App-header">
+	  <Modal basic open={letterOpened}>
+	  <Header content='Lettre de W.R.'/>
+	  <Modal.Content>
+	  <p>Mon art attise les convoitises.<br/>
+	  Je sens qu'on me suit, qu'on m'observe.<br/>
+	  Je crains d'être enlevé et emmené au terrible Fort Boyard.</p>
+	  <p>Sa façade touristique cache une prison maléfique, où tous nos dons sont éternellement volés.</p>
+	  <p>Toutes mes connaissances ne sont pas perdues. J'ai pu les regrouper dans une extraordinaire recette unique, cachée sur un serveur informatique.</p>
+	  <p>De multiples codes sont nécessaires pour y accéder.<br/>
+	  Cherchez ici, trouvez les et redévoilez au monde le vrai goût du bON.</p>
+	  <p>N'essayez pas de me retrouver, l'amnésie provoquée par le Fort est irréversible et je suis condamné à faire des plats immangeables pour le restant de mes Jours.</p>
+	  <p>Pouvez-vous juste m'envoyer ma valise déjà prête ?</p>
+	  <footer>Willy Roveli</footer>
+	  </Modal.Content>
+	  <Modal.Actions>
+	  	<Button color='green' onClick={this.handleLetterClose} inverted>
+	  		<Icon name='checkmark'/> J'y vais !
+	  	</Button>
+	  </Modal.Actions>
+	  </Modal>
         <Grid>
+	  <Grid.Row columns={1}>
+	  	<Grid.Column>
+	  		<Button color='green' onClick={this.handleLetterOpen}>
+	  			<Icon name='checkmark'/> Revoir la lettre
+	  		</Button>
+	  	</Grid.Column>
+	  </Grid.Row>
 	  <Grid.Row columns={1}>
 	    <Grid.Column>
           { locked && <Icon name='lock' color='red' size='large'/> }
